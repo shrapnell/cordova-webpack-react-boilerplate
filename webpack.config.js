9ee7f754ce6e25771cebca97d6d7367e8d5083e0
shrapnell <shr4pnell@gmail.com>
2017-10-10 14:36:46 +0200

    init
    First push

---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+var path = require('path');
+
+module.exports = {
+  entry: './www/js/App.jsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'www')
+  },
+  module: {
+    rules: [
+      // es6 js umwandeln
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      },
+      {
+        test: /\.global\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          }
+        ]
+      },
+      {
+        test: /^((?!\.global).)*\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            }
+          },
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loader: 'style!css!postcss',
+
+      },
+      // Add SASS support  - compile all .global.scss files and pipe it to style.css
+      {
+        test: /\.global\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      // Add SASS support  - compile all other .scss files and pipe it to style.css
+      {
+        test: /^((?!\.global).)*\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]__[hash:base64:5]',
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      // bilder laden
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: 'file-loader'
+      },
+      // schriften laden
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      }
+    ]
+  },
+  watch: false,
+  devServer: {
+    contentBase: path.join(__dirname, "www"),
+    compress: true,
+    port: 8080,
+    headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials": "true" 
+        }
+  }
+
+};
